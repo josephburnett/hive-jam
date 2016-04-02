f3e21ef36b64dceb3b002ca506824db48b2f33e9
Joseph Burnett <josephburnett79@gmail.com>
2016-04-01 21:48:35 -0700

    Figwheel Om template sonic-jam.

---
+++ sonic-jam/src/sonic_jam/core.cljs
@@ -0,0 +1,26 @@
+(ns sonic-jam.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(om/root
+  (fn [data owner]
+    (reify om/IRender
+      (render [_]
+        (dom/h1 nil (:text data)))))
+  app-state
+  {:target (. js/document (getElementById "app"))})
+
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
