545f6e35a22973ed3ee502daffddc4e2a8ebc1c7
Joseph Burnett <josephburnett79@gmail.com>
2016-04-18 21:33:05 -0700

    Dispatch samples with fx.

--- sonic-pi/dispatch.rb
+++ sonic-pi/dispatch.rb
@@ -1,19 +1,27 @@
 # DISPATCH
 
 define :dispatch do |note|
-
+  
   if note[:sample]
     s = note[:sample].to_sym
     note.delete(:sample)
-    sample s, **note
+    if note[:fx]
+      fx = note[:fx]
+      note.delete(:fx)
+      with_fx fx.to_sym do
+        sample s, **note
+      end
+    else
+      sample s, **note
+    end
     return
   end
-
+  
   if note[:play]
     p = note[:play]
     note.delete(:play)
     play p, **note
     return
   end
-
+  
 end
