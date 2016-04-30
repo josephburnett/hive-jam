46df29b5c8d5e4a41d4597b3ef02ab6fa4732dcf
Joseph Burnett <josephburnett79@gmail.com>
2016-04-30 00:37:28 -0700

    New data model with beats per cell (bpc).

--- sonic-pi/sandbox.rb
+++ sonic-pi/sandbox.rb
@@ -2,19 +2,24 @@
 
 drop_all_state
 
-set_state :beat,
-[
-  { beats: [1, 0, 1], sample: :drum_bass_hard, pitch: 12, amp: 1.2 },
-  { beats: [0, 1, 1, 1], sample: :drum_cymbal_closed, rpitch: 12 },
-  { beats: [0, 0, 1, 0, 0, 0, 0, 1], sample: :drum_snare_soft, amp: 1.4, rate: 3 },
-  { beats: [0, 0, 0, 0, 0, 0, 0, 1], play: 27, amp: 5.0, release: 0.1, attack: 0.01, sustain: 0.4 },
-]
+set_state :root,
+{
+  id: "guid",
+  bpc: "1/4",
+  tracks:
+  [
+    { beats: [1, 0, 1], type: "sample", sample: :drum_bass_hard, params: {pitch: 12, amp: 1.2 }},
+    { beats: [0, 1, 1, 1], type: "sample", sample: :drum_cymbal_closed, params: {rpitch: 12 }},
+    { beats: [0, 0, 1, 0, 0, 0, 0, 1], type: "sample", sample: :drum_snare_soft, params: {amp: 1.4, rate: 3 }},
+    { beats: [0, 0, 0, 0, 0, 0, 0, 1], type: "play", play: 27, params: {amp: 5.0, release: 0.1, attack: 0.01, sustain: 0.4 }},
+  ]
+}
 
-X = [0].cycle(7).to_a + [1]
-O = [0].cycle(8).to_a
+##| X = [0].cycle(7).to_a + [1]
+##| O = [0].cycle(8).to_a
 
-set_state :errie,
-[
-  { beats: X + X + X + O, sample: :ambi_piano, rpitch: 0, amp: 2.0 },
-  { beats: O + O + O + X, sample: :ambi_piano, rpitch: -2, amp: 2.0 },
-]
+##| set_state :errie,
+##| [
+##|   { beats: X + X + X + O, sample: :ambi_piano, rpitch: 0, amp: 2.0, fx: :distortion },
+##|   { beats: O + O + O + X, sample: :ambi_piano, rpitch: -2, amp: 2.0, fx: :distortion },
+##| ]
--- sonic-pi/server.rb
+++ sonic-pi/server.rb
@@ -1,182 +1,162 @@
 require 'thread'
 
-REBOOT = true
-
 # TIME
 
-bpm = 450
+define :res do Rational('1/8') end
 
-live_loop :beat_loop do
-  cue :beat
-  sleep 60.0/bpm
-end
-
-# EXEC
-
-defonce :exec, override: REBOOT do
-  beat = 0
-  in_thread do
-    loop do
-      sync :beat
-      beat += 1
-      begin
-        read_note(beat)
-      rescue Exception => e
-        puts "exception in :exec:"
-        puts e.message
-        puts e.backtrace
-      end
+live_loop :main do
+  use_bpm 120
+  wait res
+  root = _state[:root]
+  if root.nil?
+    puts "[ERROR] no root state"
+    next
+  end
+  t = tick
+  bpc = Rational(root[:bpc])
+  tpc = bpc / res
+  boundary = t % tpc.ceil
+  i = (t / tpc).ceil
+  #puts "[DEBUG] t: #{t} bpc: #{bpc} tpc: #{tpc} boundary: #{boundary} i: #{i}"
+  if boundary != 0
+    next
+  end
+  root[:tracks].each do |track|
+    on = (bools *track[:beats])[i]
+    if on
+      dispatch track
     end
   end
 end
 
-define :read_note do |beat|
-  if not defined? _state
-    puts "state component missing"
+# DISPATCH
+
+define :dispatch do |track|
+  
+  #puts "[DEBUG] dispatching track #{track}"
+  
+  type = track[:type]
+  if type.nil?
+    puts "[ERROR] no type for track #{track}"
     return
   end
-  for ns in _state.keys do
-      staff = _state[ns]
-      if not staff
-        puts "#{ns} missing"
-        next
-      end
-      puts staff
-      if not staff.respond_to? :each_with_index
-        puts "#{ns} :staff not iterable"
-        next
-      end
-      staff.each_with_index do |bar, i|
-        beats = bar[:beats]
-        if not beats
-          puts "#{ns} :staff #{i} :beats missing"
-          next
-        end
-        if (bools *beats)[beat]
-          bar_copy = Marshal.load(Marshal.dump(bar))
-          bar_copy.delete(:beats)
-          dispatch bar_copy
-        end
-      end
-    end
+  params = track[:params]
+  if params.nil?
+    puts "[ERROR] no params for track #{track}"
+    return
   end
   
-  # DISPATCH
-  
-  define :dispatch do |note|
-    
-    if note[:sample]
-      s = note[:sample].to_sym
-      note.delete(:sample)
-      if note[:fx]
-        fx = note[:fx]
-        note.delete(:fx)
-        with_fx fx.to_sym do
-          sample s, **note
-        end
-      else
-        sample s, **note
-      end
+  if type == "sample"
+    s = track[:sample].to_sym
+    if s.nil?
+      puts "[ERROR] no sample for track #{track}"
       return
     end
-    
-    if note[:play]
-      p = note[:play]
-      note.delete(:play)
-      play p, **note
-      return
+    fx = track[:fx]
+    if not fx.nil?
+      with_fx fx.to_sym do
+        sample s, **params
+      end
+    else
+      sample s, **params
     end
-    
+    return
   end
   
-  # STATE
-  
-  define :mutex do
-    Mutex.new
+  if type == "play"
+    play p, **params
+    return
   end
   
-  defonce :_state do
-    {}
-  end
-  
-  define :set_state do |ns, state|
-    if _ns_ok(ns)
-      mutex.synchronize {
-        _state[ns] = state
-      }
-      send_state_json("*", ns)
-    end
-  end
-  
-  define :drop_state do |ns|
-    if _ns_ok(ns)
-      mutex.synchronize {
-        _state.delete(ns)
-      }
-      send_state_json("*", ns)
-    end
-  end
-  
-  define :drop_all_state do
+end
+
+# STATE
+
+define :mutex do
+  Mutex.new
+end
+
+defonce :_state do
+  {}
+end
+
+define :set_state do |ns, state|
+  if _ns_ok(ns)
     mutex.synchronize {
-      _state.clear
+      _state[ns] = state
     }
+    send_state_json("*", ns)
   end
-  
-  define :get_state_json do |ns|
+end
+
+define :drop_state do |ns|
+  if _ns_ok(ns)
     mutex.synchronize {
-      return JSON.dump(_state[ns])
+      _state.delete(ns)
     }
+    send_state_json("*", ns)
   end
-  
-  define :send_state_json do |client_id, ns|
-    jam_client.send("/state", JSON.dump([client_id, ns, get_state_json(ns)]))
+end
+
+define :drop_all_state do
+  mutex.synchronize {
+    _state.clear
+  }
+end
+
+define :get_state_json do |ns|
+  mutex.synchronize {
+    return JSON.dump(_state[ns])
+  }
+end
+
+define :send_state_json do |client_id, ns|
+  jam_client.send("/state", JSON.dump([client_id, ns, get_state_json(ns)]))
+end
+
+define :_ns_ok do |ns|
+  if not ns or not ns.is_a? Symbol
+    puts "ns #{ns} is not a symbol"
+    return false
   end
-  
-  define :_ns_ok do |ns|
-    if not ns or not ns.is_a? Symbol
-      puts "ns #{ns} is not a symbol"
-      return false
-    end
-    return true
-  end
-  
-  # SERVER
-  
-  defonce :jam_server do
-    SonicPi::OSC::UDPServer.new(4559, use_decoder_cache: true)
-  end
-  
-  defonce :jam_client do
-    SonicPi::OSC::UDPClient.new("127.0.0.1", 4560, use_encoder_cache: true)
-  end
-  
-  jam_server.add_method("/drop-state") do |args|
-    assert(args.length == 2)
-    client_id = args[0]
-    ns = args[1].intern
-    drop_state ns
-  end
-  
-  jam_server.add_method("/set-state") do |args|
-    assert(args.length == 3)
-    client_id = args[0]
-    ns = args[1].intern
-    state = JSON.parse(args[2], symbolize_names: true)
-    set_state ns, state
-  end
-  
-  jam_server.add_method("/get-state") do |args|
-    assert(args.length == 2)
-    client_id = args[0]
-    ns = args[1].to_sym
-    send_state_json(client_id, ns)
-  end
-  
-  jam_server.add_method("/ping") do |args|
-    assert(args.length == 1)
-    drop_all_state
-    client_id = args[0]
-    jam_client.send("/pong", JSON.dump([client_id]))
-  end
-  
+  return true
+end
+
+# SERVER
+
+defonce :jam_server do
+  SonicPi::OSC::UDPServer.new(4559, use_decoder_cache: true)
+end
+
+defonce :jam_client do
+  SonicPi::OSC::UDPClient.new("127.0.0.1", 4560, use_encoder_cache: true)
+end
+
+jam_server.add_method("/drop-state") do |args|
+  assert(args.length == 2)
+  client_id = args[0]
+  ns = args[1].intern
+  drop_state ns
+end
+
+jam_server.add_method("/set-state") do |args|
+  assert(args.length == 3)
+  client_id = args[0]
+  ns = args[1].intern
+  state = JSON.parse(args[2], symbolize_names: true)
+  set_state ns, state
+end
+
+jam_server.add_method("/get-state") do |args|
+  assert(args.length == 2)
+  client_id = args[0]
+  ns = args[1].to_sym
+  send_state_json(client_id, ns)
+end
+
+jam_server.add_method("/ping") do |args|
+  assert(args.length == 1)
+  drop_all_state
+  client_id = args[0]
+  jam_client.send("/pong", JSON.dump([client_id]))
+end
