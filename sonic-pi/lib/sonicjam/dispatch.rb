2b5abc0dd087460d5fd15ccbb28afa15f7d7bcc2
Joseph Burnett <josephburnett79@gmail.com>
2016-06-06 00:20:29 -0700

    Dispatch._dispatch_grid and tests.

--- sonic-pi/lib/sonicjam/dispatch.rb
+++ sonic-pi/lib/sonicjam/dispatch.rb
@@ -8,12 +8,65 @@ module SonicJam
     end
 
     def dispatch(tick)
-      root = state.get_state :root
-      _dispatch_grid tick, root
+      root = @state.get_state :root
+      _dispatch_grid root, tick
     end
 
-    def _dispatch_grid(tick, grid, options={})
-      pass
+    def _dispatch_grid(grid, tick, parent_track={})
+      bpc = grid[:bpc]
+      thunks = []
+      grid[:tracks].each_index do |index|
+        track = grid[:tracks][index]
+        type = track[:type]
+        beats = track[:beats]
+        on, boundary = SonicJam::_on_the_beat?(bpc, @resolution, beats, tick)
+
+        # Apply inheritance from parent track
+        synth = track.fetch(:synth, parent_track.fetch(:synth, nil))
+        synth_params = track.fetch(:'synth-params', {})
+        synth_params = parent_track.fetch(:'synth-params', {}).merge(synth_params)
+        sample = track.fetch(:sample, parent_track.fetch(:sample, nil))
+        sample_params = track.fetch(:'sample-params', {})
+        sample_params = parent_track.fetch(:'sample-params', {}).merge(sample_params)
+        fx = track.fetch(:fx, [])
+        fx = parent_track.fetch(:fx, []) + fx
+
+        case type
+        when "grid"
+          if not on
+            next
+          end
+          grid_id = track[:id].to_sym
+          sub_grid = @state.get_state(grid_id)
+          thunks += _dispatch_grid sub_grid, tick, {
+                                    :'synth-params' => synth_params,
+                                    :'sample-params' => sample_params,
+                                    fx: fx
+                                  }
+        when "synth"
+          if not on and boundary
+            next
+          end
+          thunks.push(lambda { _dispatch_synth synth, synth_params, fx })
+        when "sample"
+          if not on and boundary
+            next
+          end
+          thunks.push(lambda { _dispatch_sample sample, sample_params, fx })
+        else
+          # do nothing
+        end
+      end
+
+      return thunks
+    end
+
+    def _dispatch_synth(synth, params, fx)
+      # TODO
+    end
+
+    def _dispatch_sample(sample, params, fx)
+      # TODO
     end
   end
 
--- sonic-pi/test/test_dispatch.rb
+++ sonic-pi/test/test_dispatch.rb
@@ -42,6 +42,88 @@ module SonicJam
       assert_equal expect_bound, bound,
                    "Expected bound #{expect_bound} but got #{bound} for #{bpc}, #{res}, #{beats} at #{tick}"
     end
+
+    def test_dispatch_grid_simple
+      @state.set_state({ id: "a", name: "a", bpc: "1",
+                         tracks: [
+                           { type: "synth", synth: "synth", beats: [[1]] }
+                         ]
+                       })
+      @state.set_state({ id: "root", name: "root", bpc: "1",
+                         tracks: [
+                           { type: "grid", id: "a", beats: [[1]] }
+                         ]
+                       })
+      check_dispatch_grid [
+        {
+          synth: "synth",
+          params: {},
+          fx: [],
+        }
+      ]
+    end
+
+    def test_dispatch_grid_param_inheritance
+      @state.set_state({ id: "a", name: "a", bpc: "1",
+                         tracks: [
+                           { type: "synth", synth: "synth-1", beats: [[1]] },
+                           { type: "synth", synth: "synth-2", beats: [[1]],
+                             :'synth-params' => { from: "grid-a-1" }},
+                           { type: "sample", sample: "sample-1", beats: [[1]] },
+                           { type: "sample", sample: "sample-2", beats: [[1]],
+                             :'sample-params' => { from: "grid-a-3" }},
+                         ]
+                       })
+      @state.set_state({ id: "b", name: "b", bpc: "1",
+                         tracks: [
+                           { type: "grid", id: "a", beats: [[1]],
+                             :'synth-params' => { from: "grid-b-0" },
+                             :'sample-params' => { from: "grid-b-0" },}
+                         ]
+                       })
+      @state.set_state({ id: "root", name: "root", bpc: "1",
+                         tracks: [
+                           { type: "grid", id: "b", beats: [[1]] }
+                         ]
+                       })
+      check_dispatch_grid [
+        {
+          synth: "synth-1",
+          params: { from: "grid-b-0" },
+          fx: [],
+        },
+        {
+          synth: "synth-2",
+          params: { from: "grid-a-1" },
+          fx: [],
+        },
+        {
+          sample: "sample-1",
+          params: { from: "grid-b-0" },
+          fx: [],
+        },
+        {
+          sample: "sample-2",
+          params: { from: "grid-a-3" },
+          fx: [],
+        }
+      ]
+    end
+    
+    def check_dispatch_grid(expected)
+      @dispatch.instance_variable_set(:@calls, [])
+      def @dispatch._dispatch_synth(synth, params, fx)
+        @calls.push({ synth: synth, params: params, fx: fx })
+      end
+      def @dispatch._dispatch_sample(sample, params, fx)
+        @calls.push({ sample: sample, params: params, fx: fx })
+      end
+      thunks = @dispatch.dispatch(0)
+      thunks.each do |thunk|
+        thunk.call()
+      end
+      assert_equal expected, @dispatch.instance_variable_get(:@calls)
+    end
     
   end
 end
