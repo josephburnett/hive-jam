18866bcec75fdf40cf6359c539ff2bd87445939b
Joseph Burnett <josephburnett79@gmail.com>
2016-06-10 21:16:39 -0700

    Params for lambdas and more granular inheritance.

---
+++ sonic-pi/lib/sonicjam/params.rb
@@ -0,0 +1,53 @@
+module SonicJam
+
+  class Params
+
+    def initialize(hash, parent=nil)
+      @hash = hash
+      @parent = parent
+      @keys = Set.new(@hash.keys)
+      if not @parent.nil?
+        @keys.merge(@parent._keys)
+      end
+    end
+
+    def materialize(bind)
+      materialized_params = {}
+      @keys.each do |key|
+        materialized_params[key] = _materialize_key(bind, key)
+      end
+      return materialized_params
+    end
+
+    def _materialize_key(bind, key, child=nil)
+      bind = _local_variable_set(bind, :child, child)
+      value = @hash.fetch(key, child)
+      if not value.nil? and value.is_a? String
+        if value.start_with?("!")
+          # '!' breaks the inheritance chain
+          return value[1..-1]
+        end
+        if value.start_with?("\\")
+          value = eval value[1..-1], bind
+        end
+      end
+      if not @parent.nil?
+        value = @parent._materialize_key(bind, key, value)
+      end
+      return value
+    end
+    
+    def _keys
+      return @keys
+    end
+
+    # TODO: Replace with binding.local_variable_set, available in Ruby 2.2
+    def _local_variable_set(bind, variable, value)
+      if value
+        bind = bind.eval("#{variable.to_s} = #{value}; binding")
+      end
+      return bind
+    end
+    
+  end
+end
---
+++ sonic-pi/test/test_params.rb
@@ -0,0 +1,44 @@
+require "minitest/autorun"
+
+require_relative "../lib/sonicjam/params"
+
+module SonicJam
+  class TestParams < Minitest::Unit::TestCase
+
+    def setup
+    end
+
+    def test_simple_hash
+      params = Params.new({a: 1, b: 2})
+      assert_equal({a: 1, b: 2}, params.materialize(binding))
+    end
+
+    def test_inherit_scalar
+      params = Params.new({a: 1, b: 1}, Params.new({a: 2}))
+      assert_equal({a: 2, b: 1}, params.materialize(binding))
+    end
+
+    def test_inheritance_break
+      params = Params.new({a: "!foo"}, Params.new({a: "bar"}))
+      assert_equal({a: "foo"}, params.materialize(binding))
+    end
+
+    def test_lambda
+      params = Params.new({a: "\\ 1+1"})
+      assert_equal({a: 2}, params.materialize(binding))
+    end
+
+    def test_lambda_binding
+      a = 1
+      params = Params.new({a: "\\ a"})
+      assert_equal({a: 1}, params.materialize(binding))
+    end
+
+    def test_lambda_inheritance
+      a = 2
+      params = Params.new({a: "\\ a"}, Params.new({a: "\\ 2 * child"}))
+      assert_equal({a: 4}, params.materialize(binding))
+    end
+    
+  end
+end
