c59c0d7ca7dba1b655920447a604eb5923ec9e19
Joseph Burnett <josephburnett79@gmail.com>
2016-06-19 22:18:01 -0700

    Build script to generate embedded go assets.

--- osc-bridge/bootstrap/bootstrap.go
+++ osc-bridge/bootstrap/bootstrap.go
@@ -1,54 +1,60 @@
 package bootstrap
 
 import "github.com/josephburnett/sonic-jam/osc-bridge/common"
+import "github.com/josephburnett/sonic-jam/osc-bridge/data"
 import "github.com/scgolang/osc"
-import "io/ioutil"
 import "log"
-import "time"
 
 var oscClient = common.Connect("127.0.0.1", 4557)
 
-func serverUp() bool {
-	oscMessage, err := osc.NewMessage("/ping")
+func send(address, message string) error {
+	oscMessage, err := osc.NewMessage(address)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	oscMessage.WriteString("sonicjam/bootstrap")
-	for i := 0; i < 10; i++ {
-		err = oscClient.Send(oscMessage)
-		if err != nil {
-			log.Print(err)
-			return false
-		}
+	if message != "" {
+		oscMessage.WriteString(message)
 	}
-	log.Print("Sonic Pi is up.")
-	return true
-}
-
-func upload(filename string) {
-	file, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
-	oscMessage, err := osc.NewMessage("/run-code")
-	if err != nil {
-		panic(err)
-	}
-	oscMessage.WriteString("sonicjam/bootstrap")
-	oscMessage.WriteString(string(file))
 	err = oscClient.Send(oscMessage)
 	if err != nil {
-		panic(err)
+		return err
 	}
+	return nil
+}
+
+func ping() error {
+	return send("/ping", "")
+}
+
+func upload(filename string) error {
+	data, err := data.Asset(filename)
+	if err != nil {
+		return err
+	}
+	return send("/run-code", string(data))
+}
+
+var files = [...]string{
+	"sonic-pi/lib/sonicjam/state.rb",
+	"sonic-pi/lib/sonicjam/params.rb",
+	"sonic-pi/lib/sonicjam/dispatch.rb",
+	"sonic-pi/server.rb",
 }
 
 func Boot() {
-	for !serverUp() {
-		log.Print("Have you started Sonic Pi?")
-		time.Sleep(5 * time.Second)
+	for i := 0; i < 10; i++ {
+		err := ping()
+		if err != nil {
+			log.Print("Have you started Sonic Pi?")
+			panic(err)
+		}
 	}
-	upload("/Users/josephburnett/sonic-jam/sonic-pi/lib/sonicjam/state.rb")
-	upload("/Users/josephburnett/sonic-jam/sonic-pi/lib/sonicjam/params.rb")
-	upload("/Users/josephburnett/sonic-jam/sonic-pi/lib/sonicjam/dispatch.rb")
-	upload("/Users/josephburnett/sonic-jam/sonic-pi/server.rb")
+	for _, file := range files {
+		err := upload(file)
+		if err != nil {
+			log.Print("Error uploading file ", file)
+			panic(err)
+		}
+	}
 }
