46b6351ff470e3acaa94a6dd3891a2824fc55bc2
Joseph Burnett <josephburnett79@gmail.com>
2016-08-30 04:16:41 +0000

    Bind client to :js-client-host and server to 0.0.0.0

--- sonic-jam/project.clj
+++ sonic-jam/project.clj
@@ -24,18 +24,14 @@
   :cljsbuild {:builds
               [{:id "dev"
                 :source-paths ["src"]
-
-                ;; If no code is to be run, set :figwheel true for continued automagical reloading
-                :figwheel {:on-jsload "sonic-jam.core/on-js-reload"}
+                :figwheel {:websocket-host :js-client-host
+                           :on-jsload "sonic-jam.core/on-js-reload"}
 
                 :compiler {:main sonic-jam.core
                            :asset-path "js/compiled/out"
                            :output-to "resources/public/js/compiled/sonic_jam.js"
                            :output-dir "resources/public/js/compiled/out"
                            :source-map-timestamp true}}
-               ;; This next build is an compressed minified build for
-               ;; production. You can build this with:
-               ;; lein cljsbuild once min
                {:id "min"
                 :source-paths ["src"]
                 :compiler {:output-to "resources/public/js/compiled/sonic_jam.js"
@@ -43,34 +39,5 @@
                            :optimizations :advanced
                            :pretty-print false}}]}
 
-  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
-
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             }
-  :profiles {:dev-gce {:figwheel {:server-ip "0.0.0.0"}}})
+  :figwheel {:server-ip "0.0.0.0"
+             :css-dirs ["resources/public/css"]})
