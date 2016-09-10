35d04499938a869500b58a46e3ccb3db5d9926c7
Joseph Burnett <josephburnett79@gmail.com>
2016-09-10 23:54:16 +0000

    DeNom (inverse of NomNom).

--- golang/src/hivejam/state/nomnom.go
+++ golang/src/hivejam/state/nomnom.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
-	
+
 	"github.com/attic-labs/noms/go/types"
 )
 
@@ -15,8 +15,9 @@ func NomNom(a interface{}) (types.Value, error) {
 	switch k {
 	case reflect.Bool:
 		return types.Bool(v.Bool()), nil
-	case reflect.Int:
-		return types.Number(v.Int()), nil
+	// TODO: encode integers are strings because Noms Number type is float64
+	// case reflect.Int:
+	// 	return types.Number(v.Int()), nil
 	case reflect.Float64:
 		return types.Number(v.Float()), nil
 	case reflect.String:
@@ -63,4 +64,82 @@ func NomNom(a interface{}) (types.Value, error) {
 		return types.NewStruct(name, nomsFields), nil
 	}
 	return nil, errors.New(fmt.Sprintf("Unsupported kind: %v", k))
+}
+
+func DeNom(v types.Value, a interface{}) error {
+	t := reflect.TypeOf(a)
+	vr := reflect.ValueOf(&a)
+	k := t.Kind()
+	switch k {
+	case reflect.Bool:
+		nomsBool, ok := v.(types.Bool)
+		if !ok {
+			return typeMatchError(v, a)
+		}
+		vr.Elem().SetBool(bool(nomsBool))
+	case reflect.Float64:
+		nomsNumber, ok := v.(types.Number)
+		if !ok {
+			return typeMatchError(v, a)
+		}
+		vr.Elem().SetFloat(float64(nomsNumber))
+	case reflect.String:
+		nomsString, ok := v.(types.String)
+		if !ok {
+			return typeMatchError(v, a)
+		}
+		vr.Elem().SetString(string(nomsString))
+	case reflect.Slice:
+		nomsList, ok := v.(types.List)
+		if !ok {
+			return typeMatchError(v, a)
+		}
+		for _, nomsValue := range nomsList.ChildValues() {
+			elementType := reflect.TypeOf(vr.Elem()).Elem()
+			elementValue := reflect.New(elementType)
+			err := DeNom(nomsValue, elementValue)
+			if err != nil {
+				return err
+			}
+			vr.Elem().Set(reflect.Append(vr.Elem(), elementValue))
+		}
+	case reflect.Map:
+		nomsMap, ok := v.(types.Map)
+		if !ok {
+			return typeMatchError(v, a)
+		}
+		outterErr := error(nil)
+		nomsMap.Iter(func (k, v types.Value) (stop bool) {
+			keyType := reflect.TypeOf(vr.Elem()).Key()
+			keyValue := reflect.New(keyType)
+			err := DeNom(k, keyValue)
+			if err != nil {
+				outterErr = err
+				return false
+			}
+			valueType := reflect.TypeOf(vr.Elem()).Elem()
+			valueValue := reflect.New(valueType)
+			err = DeNom(v, valueValue)
+			if err != nil {
+				outterErr = err
+				return false
+			}
+			vr.Elem().SetMapIndex(keyValue, valueValue)
+			return true
+		})
+		return outterErr
+	// case reflect.Struct:
+	// 	nomsStruct, ok := v.(types.Struct)
+	// 	if !ok {
+	// 		return typeMatchError(v, a)
+	// 	}
+		
+	}
+	return errors.New(fmt.Sprintf("Unsupported kind: %v", k))
+}
+
+func typeMatchError(v, a interface{}) error {
+	vt := reflect.TypeOf(v)
+	at := reflect.TypeOf(a)
+	return errors.New(fmt.Sprintf("Expected Noms type compatible with %v but found %v.", at, vt))
 }
